65c57841c402e806b2726ca957fc36424659490f
albaniahazera <alzgamer2005@gmail.com>
2025-01-14 21:52:06 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert('Terima kasih sudah mengunjungi website portofolio saya !');
+const body = document.body;
+const div1 = document.getElementById('div1');
+const p = document.createElement('p');
+p.innerHTML = '<marquee>Terima kasih sudah mengunjungi website portofolio saya !</marquee>';
+p.style.fontFamily = 'Times New Roman';
+div1.style.backgroundColor = 'aqua';
+div1.append(p);
+
