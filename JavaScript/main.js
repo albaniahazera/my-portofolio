2adf3012923d318ccbdf4a3be91630c51e9cc44a
albaniahazera <alzgamer2005@gmail.com>
2025-02-10 20:50:35 +0700

    New Update 1.1

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,14 @@
-const body = document.body;
-const div1 = document.getElementById('div1');
-const p = document.createElement('p');
-p.innerHTML = '<marquee>Terima kasih sudah mengunjungi website portofolio saya !</marquee>';
-p.style.fontFamily = 'Times New Roman';
-div1.style.backgroundColor = 'aqua';
-div1.append(p);
+function toggleText(elementId, toggleId) {
+    const conten = document.getElementById(elementId);
+    const role = document.getElementById(toggleId);
+    conten.addEventListener("click", () => {
+        if (role.style.display === "none") {
+            role.style.display = "block";
+        }else {
+            role.style.display = "none";
+        };
+    });
+}
+
+
 
